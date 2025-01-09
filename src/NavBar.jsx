8970ad3f3f95868bb4e305c8dfd158c09fec06e0
Ammar Iqbal <aiqbal@euthyna.ai>
2025-01-09 20:18:15 +0500

    pucit to fcit

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -21,7 +21,7 @@ function NavBar() {
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Pucit
+              FCIT
             </span>
           </Link>
 
